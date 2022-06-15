30a345c2ad49bca7e7c1c127cc4da8c174682e7a
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-06-15 23:53:04 +0530

    Added implemented program for stock management.

---
+++ src/com/bridgelabz/Day11_12_OOP/Stock.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.Day11_12_OOP;
+
+public class Stock {
+    private String stockName;
+    private int price;
+    private int numberOfShare;
+
+    public String getStockName() {
+        return stockName;
+    }
+
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getNumberOfShare() {
+        return numberOfShare;
+    }
+
+    public void setNumberOfShare(int numberOfShare) {
+        this.numberOfShare = numberOfShare;
+    }
+}
+
---
+++ src/com/bridgelabz/Day11_12_OOP/StockOperationsIF.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.Day11_12_OOP;
+
+public interface StockOperationsIF {
+    public Stock addStock();
+    public  void calculateEachStockValue(Stock[] listOfstock);
+}
